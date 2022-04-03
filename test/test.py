4581ef2eb733537ee7bfe9c83589c0f4a5d9c385
Yanling Guo <aylguo@outlook.com>
2022-04-03 13:50:37 +0800

    add some comments in test.py

--- test/test.py
+++ test/test.py
@@ -9,8 +9,8 @@ def fasta_generator(inp_number):
     f = open('test.fasta', 'w')
     for num in range(0, inp_number):
         line1 = '>test_sequence_number_' + str(num + 1) + '\n'
-        lower = ''.join(random.choice('atgc') for i in range(0, 8))
-        upper = ''.join(random.choice('ATGC') for j in range(0, 7))
+        lower = ''.join(random.choice('atgc') for i in range(0, 8)) ## i is for how many lower-cased base you need
+        upper = ''.join(random.choice('ATGC') for j in range(0, 7)) ## j is for how many upper-cased base you need
         line2 = '^NN' + lower + upper
         seq = line1 + line2
         print(seq)
