9f82d3d766452ef184d5389dabcf5d228616d0bb
winterdrive <90021888+winterdrive@users.noreply.github.com>
2022-04-05 19:37:54 +0800

    Add files via upload

---
+++ merge/00_blastForRef.sh
@@ -0,0 +1,28 @@
+
+
+
+
+/home/lykuo/ncbi-blast-2.10.0+/bin/makeblastdb -in /home/lykuo/lab_data/NGS_data/miseq/LIB810_S9/fermalies_rbcL.fasta -dbtype nucl -out refDB
+
+cat /home/sktang/powerBC/r1/*.fas /home/sktang/powerBC/r2/*.fas > catQuery.fas
+
+/home/lykuo/ncbi-blast-2.10.0+/bin/blastn -db refDB -query catQuery.fas -num_threads 20 -out refResult.txt -outfmt 6
+
+
+
+# Microlepia_substrigosa_CYH20090514-016_0514-016_01_r1_0.994_abundance_2033	
+# MH319942_Dennstaedtiaceae_Histiopteris_incisa	
+# 94.656(this)	
+# 262	(this)
+# 13	
+# 1	
+# 1	
+# 262	
+# 558	
+# 818	
+# 1.44e-115	
+# 405
+# Microlepia_substrigosa_CYH20090514-0
+
+
+/home/lykuo/lab_data/NGS_data/miseq/LIB810_S9/rbcLC_demultiplex/denoice_best/r1
