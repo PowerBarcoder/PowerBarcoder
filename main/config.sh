567cc5827655f54d0e0aabb7e1a099f6b7bf04fe
winterdrive <90021888+winterdrive@users.noreply.github.com>
2023-01-07 16:42:07 +0800

    Update config.sh
    20230107

--- main/config.sh
+++ main/config.sh
@@ -1,7 +1,7 @@
 # ! /bin/bash
 
 # Where is our PowerBarcoder ? 
-workingDirectory="/home2/barcoder_test/RUN_sk_20230103/PowerBarcoder/main/"
+workingDirectory="/home2/barcoder_test/RUN_sk_20230103/PowerBarcoder/main/" #20230107 移到最底下、用參數傳進來
 # Where did you install cutadapt ? 
 myCutadaptPath="/home/lykuo/cutadapt-venv/bin/"
 # Where did you install fastp ? 
@@ -9,13 +9,14 @@ myFastpPath="/home/lykuo/miniconda2/bin/"
 # Where did you install localBlast? 
 localBlastToolDir="/home/lykuo/ncbi-blast-2.10.0+/bin/"
 # Where is your NGS Data ?
-mainDataPath="/home2/barcoder_test/RUN1.5/"
+mainDataPath="/home2/barcoder_test/RUN1.5/" #20230107可以改成ampliconInfo
 # Where do you want to generate the results ?
 resultDataPath="/home2/barcoder_test/RUN_sk_20230103/PowerBarcoder/result/"
 # where is your Miseq gz data ?
-myTrimmedR1Gz="/home/lykuo/lab_data/NGS_data/miseq/HGT22070_Amplicon_RUN1.5/trim_RUN1.5_R1.fq.gz"
-myTrimmedR2Gz="/home/lykuo/lab_data/NGS_data/miseq/HGT22070_Amplicon_RUN1.5/trim_RUN1.5_R2.fq.gz"
+myTrimmedR1Gz="/home/lykuo/lab_data/NGS_data/miseq/HGT22070_Amplicon_RUN1.5/trim_RUN1.5_R1.fq.gz" #20230107可以改成R1FastqGz
+myTrimmedR2Gz="/home/lykuo/lab_data/NGS_data/miseq/HGT22070_Amplicon_RUN1.5/trim_RUN1.5_R2.fq.gz" #20230107可以改成R2FastqGz
 
+# 單次fastp的名稱，可以改成myTrimmedR2Gz.json這種感覺，避免做多次fastp有覆蓋問題
 summaryJsonFileName="221229RUN1_5.json"
 summaryHtmlFileName="221229RUN1_5.html"
 
