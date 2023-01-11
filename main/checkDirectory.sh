bbc7899af29e0d74ca65472d1a3e659ed4192509
winston <winstontang@fontrip.com>
2023-01-11 17:18:18 +0800

    [modify] add checkDirectory.sh (15%)

---
+++ main/checkDirectory.sh
@@ -0,0 +1,39 @@
+# ! /bin/bash
+. ./config.sh
+
+echo "checking file directories..."
+
+#yixuan modified for multiLoci
+for ((i=0; i<${#nameOfLoci[@]}; i++))
+do
+
+# create Layer 1st folder
+mkdir -p ${resultDataPath}${nameOfLoci[i]}_demultiplex
+# create Layer 2nd folders
+mkdir -p ${resultDataPath}${nameOfLoci[i]}_demultiplex/trimmed
+mkdir -p ${resultDataPath}${nameOfLoci[i]}_demultiplex/denoice
+mkdir -p ${resultDataPath}${nameOfLoci[i]}_demultiplex/denoice_best
+# create Layer 3rd folders
+mkdir -p ${resultDataPath}${nameOfLoci[i]}_demultiplex/denoice/r1
+mkdir -p ${resultDataPath}${nameOfLoci[i]}_demultiplex/denoice/r2
+mkdir -p ${resultDataPath}${nameOfLoci[i]}_demultiplex/denoice_best/r1
+mkdir -p ${resultDataPath}${nameOfLoci[i]}_demultiplex/denoice_best/r2
+mkdir -p ${resultDataPath}${nameOfLoci[i]}_demultiplex/denoice/nonmerged
+mkdir -p ${resultDataPath}${nameOfLoci[i]}_demultiplex/denoice_best/nonmerged
+# create Layer 4th folders
+mkdir -p ${resultDataPath}${nameOfLoci[i]}_demultiplex/denoice/nonmerged/r1
+mkdir -p ${resultDataPath}${nameOfLoci[i]}_demultiplex/denoice/nonmerged/r2
+mkdir -p ${resultDataPath}${nameOfLoci[i]}_demultiplex/denoice/nonmerged/r1Ref
+mkdir -p ${resultDataPath}${nameOfLoci[i]}_demultiplex/denoice/nonmerged/r2Ref
+mkdir -p ${resultDataPath}${nameOfLoci[i]}_demultiplex/denoice/nonmerged/mergeSeq
+mkdir -p ${resultDataPath}${nameOfLoci[i]}_demultiplex/denoice/nonmerged/aligned
+mkdir -p ${resultDataPath}${nameOfLoci[i]}_demultiplex/denoice_best/nonmerged/r1
+mkdir -p ${resultDataPath}${nameOfLoci[i]}_demultiplex/denoice_best/nonmerged/r2
+mkdir -p ${resultDataPath}${nameOfLoci[i]}_demultiplex/denoice_best/nonmerged/r1Ref
+mkdir -p ${resultDataPath}${nameOfLoci[i]}_demultiplex/denoice_best/nonmerged/r2Ref
+mkdir -p ${resultDataPath}${nameOfLoci[i]}_demultiplex/denoice_best/nonmerged/mergeSeq
+mkdir -p ${resultDataPath}${nameOfLoci[i]}_demultiplex/denoice_best/nonmerged/aligned
+# create Layer 5th folders
+mkdir -p ${resultDataPath}${nameOfLoci[i]}_demultiplex/denoice_best/nonmerged/aligned/mafft
+
+done
--- main/powerBarcode.sh
+++ main/powerBarcode.sh
@@ -4,6 +4,11 @@
 bash checkRequirement.sh
 
 echo "environment OK !"
+
+bash checkDirectory.sh
+
+echo "file directories are created !"
+
 echo "Start running PowerBarcode !"
 
 bash illumina_PE_demultiplex_all_newprimer.sh
