0bf0835acdcbb7ff5d4822a63e39f383d60b5c84
winston <winstontang@fontrip.com>
2023-01-13 13:40:33 +0800

    [modify] rollback config.sh for alignment optimization (20%)

--- main/config.sh
+++ main/config.sh
@@ -9,7 +9,7 @@ localBlastToolDir="/home/lykuo/ncbi-blast-2.10.0+/bin/"
 # Where is your NGS Data ?
 ampliconInfo="/home2/barcoder_test/RUN1.5/" #20230107可以改成ampliconInfo  done
 # Where do you want to generate the results ?
-resultDataPath="/home2/barcoder_test/RUN_sk_20230111_10N/PowerBarcoder/result/"
+resultDataPath="/home2/barcoder_test/RUN_sk_20230103/PowerBarcoder/result/"
 # where is your Miseq gz data ?
 R1FastqGz="/home/lykuo/lab_data/NGS_data/miseq/HGT22070_Amplicon_RUN1.5/trim_RUN1.5_R1.fq.gz" #20230107可以改成R1FastqGz done
 R2FastqGz="/home/lykuo/lab_data/NGS_data/miseq/HGT22070_Amplicon_RUN1.5/trim_RUN1.5_R2.fq.gz" #20230107可以改成R2FastqGz done
@@ -78,6 +78,6 @@ minimumLengthCutadaptorInLoop+=(150)
 customizedThreadNumber+=(30)
 
 # Where is our PowerBarcoder ?
-workingDirectory="/home2/barcoder_test/RUN_sk_20230111_10N/PowerBarcoder/main/" #20230107 移到最底下、用參數傳進來 done
+workingDirectory="/home2/barcoder_test/RUN_sk_20230103/PowerBarcoder/main/" #20230107 移到最底下、用參數傳進來 done
 
 echo 'params imported!'
