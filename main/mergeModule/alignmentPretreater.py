38f7aaa68093241c6a750b4fb4d153522aa9cc02
WINSTON\kwz50 <kwz50902@gmail.com>
2023-02-06 22:06:52 +0800

    [review] rbcLC 10N 35% 測alignment結果

--- main/mergeModule/alignmentPretreater.py
+++ main/mergeModule/alignmentPretreater.py
@@ -6,6 +6,46 @@ from subprocess import PIPE
 import os
 import sys
 
+# 製造待測序列路徑的方法
+def qseqidFile(outputLoadpath,rWho,fileName):
+    qseqidFileDir=outputLoadpath+rWho+"/"
+    qseqidFileName=fileName
+    qseqidFile=qseqidFileDir+qseqidFileName
+    return qseqidFile
+
+
+"""
+# 先定義一個判斷正負的方法
+2022年的做法是看12跟13相乘：
+相乘為正就是positive，為負就是negative
+20230206-10N：(沒很懂)
+用 subject start 與 end 相減 正值或負值
+ 再與 （query start 與 end 相減）相乘決定 是否方向一至
+"""
+def negativeTest(a,b):
+    if ((a[0]=="-")and(b[0]=="-"))or ((a[0]!="-")and(b[0]!="-")):
+        return "positive"
+    elif ((a[0]!="-")and(b[0]=="-")) or ((a[0]=="-")and(b[0]!="-")):
+        return "negative"
+
+# 反轉序列的方法
+def ReverseComplement(seq):
+    # """Return reverse complement fastaID_seq_dic, ignore gaps"""
+    seq = seq.replace(' ','') #Remove space
+    seq = seq.replace('\n','') #Remove LF
+    seq = seq[::-1] #Reverse the fastaID_seq_dic
+    basecomplement = {'A': 'T', 'C': 'G', 'G': 'C', 'T': 'A', 'N': 'N', 'R': 'Y', 'Y':'R', 'M': 'K', 'K': 'M', 'S': 'S', 'W': 'W', 'H': 'D', 'B': 'V'} #Make a dictionary for complement
+    letters = list(seq) #Turn the fastaID_seq_dic into a list
+    letters = [basecomplement[base] for base in letters]
+    return ''.join(letters)+'\n' #Turn the list into a string
+
+# 兩條序列匯出成一檔的方法
+def createRefFile(rWho,rWhoRowList):
+    with open (outputLoadpath+rWho+"Ref/"+qseqid,"w") as RefFile:
+        finalRowList=rWhoRowList+targetRowList
+        RefFile.writelines(finalRowList)
+        # print(RefFile.read())
+
 print("alignmentPretreater.py is running on loci: "+sys.argv[4])
 
 # loadpath="/home/sktang/powerBC/"
@@ -13,18 +53,11 @@ localBlastLoadpath=sys.argv[3]
 outputLoadpath=sys.argv[3]+sys.argv[4]+"_demultiplex/denoice_best/nonmerged/"
 
 
-# localblast完的序列
+# localblast完的序列檔案
 fastaFileDir=localBlastLoadpath+"blastResult/"
 fastaFileName=sys.argv[4]+"_blastResult.txt"
 fastaFile=fastaFileDir+fastaFileName
 # print(fastaFile)
-
-# 待測序列
-def qseqidFile(outputLoadpath,rWho,fileName):
-    qseqidFileDir=outputLoadpath+rWho+"/"
-    qseqidFileName=fileName
-    qseqidFile=qseqidFileDir+qseqidFileName
-    return qseqidFile
 
 # ref
 # sseqidFileDir="/home/lykuo/lab_data/NGS_data/miseq/LIB810_S9/"
@@ -34,39 +67,9 @@ sseqidFileName=sys.argv[2]
 
 sseqidFile=sseqidFileDir+sseqidFileName
 
-
-# # vscode測試用路徑，上機時註解
-# loadpath="C:/Users/kwz50/"
-# fastaFile=loadpath+"blastResult.txt"
-# sseqidFile=loadpath+"fermalies_rbcL.fasta"
-
-# 先定義一個判斷正負的方法
-def negativeTest(a,b):
-    if ((a[0]=="-")and(b[0]=="-"))or ((a[0]!="-")and(b[0]!="-")):
-        return "positive"
-    elif ((a[0]!="-")and(b[0]=="-")) or ((a[0]=="-")and(b[0]!="-")):
-        return "negative"
-
-# 反轉序列的方法
-def ReverseComplement(seq):
-	# """Return reverse complement fastaID_seq_dic, ignore gaps"""
-	seq = seq.replace(' ','') #Remove space
-	seq = seq.replace('\n','') #Remove LF
-	seq = seq[::-1] #Reverse the fastaID_seq_dic
-	basecomplement = {'A': 'T', 'C': 'G', 'G': 'C', 'T': 'A', 'N': 'N', 'R': 'Y', 'Y':'R', 'M': 'K', 'K': 'M', 'S': 'S', 'W': 'W', 'H': 'D', 'B': 'V'} #Make a dictionary for complement
-	letters = list(seq) #Turn the fastaID_seq_dic into a list
-	letters = [basecomplement[base] for base in letters] 
-	return ''.join(letters)+'\n' #Turn the list into a string
-
-# 兩條序列匯出成一檔的方法
 r1RowList=[]
 r2RowList=[]
 targetRowList=[]
-def createRefFile(rWho,rWhoRowList):
-    with open (outputLoadpath+rWho+"Ref/"+qseqid,"w") as RefFile:
-        finalRowList=rWhoRowList+targetRowList
-        RefFile.writelines(finalRowList)
-        # print(RefFile.read())
 
 with open(fastaFile,"r")as file:
     lines=file.readlines()
@@ -119,9 +122,7 @@ with open(fastaFile,"r")as file:
             targetRowList+=(lines[targetRowNumber:targetRowNumber+2])    
             # print(targetRowList)
 
-# if else判斷，如果多行的fasta在處理成一行的
-
-
+        # if else判斷，如果多行的fasta在處理成一行的
         if ((sign=="negative")and(forword=="r1")):
             targetRowList[1]=ReverseComplement(targetRowList[1])
             r2RowList[1]=ReverseComplement(r2RowList[1])
