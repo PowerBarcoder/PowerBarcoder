40a22257f65a145e72c296bbc9513f52338f480f
WINSTON\kwz50 <kwz50902@gmail.com>
2023-02-13 00:13:54 +0800

    [modify] rbcL 10N 53% 整理TODO，10N r file test

--- main/dada2_denoise_PE_newprimer.r
+++ main/dada2_denoise_PE_newprimer.r
@@ -33,8 +33,8 @@ numbers = c("01", "02", "03", "04", "05", "06", "07", "08", "09", 10:99)
 # 多loci，要從sh傳參近來，搭配multiplex_clean.txt一起使用，後者是使用者自備的，所以格式很重要
 # trnLF = c("L5675", "F4121")
 # trnL = c("oneIf1", "L7556")
-AP_minlength = 270
-
+AP_minlength = 500  #TODO 這個也變參數
+minoverlap = 4 #TODO 這個也變參數
 #AP<-data.frame(rbcLC, rbcLN, trnLF, trnL)
 #AP<-data.frame(rbcLC)
 
@@ -56,17 +56,15 @@ multiplex <- read.table(
 #1:ncol(AP) to loop all regions
 for (a in 1:ncol(AP)){
   colnames(AP[a]) -> region
+  colnames(AP[a]) -> region
   AP[,a][1] -> Fp
   AP[,a][2] -> Rp
-
-
-  # 檢查amplicon欄為空，則刪除table內該列資料
   multiplex[!multiplex[,AP[,a][2]] %in% "",] -> amplicon
-
-
   miss = c()
   seqtable = c()
-  mergfail = c()
+  dadamergfail = c()
+  merg = c() #Kuo_modified
+  nonmerg = c() #Kuo_modified
   path_demultiplex = paste0(path_result, region, "_demultiplex")
   path_trim <- paste0(path_demultiplex, "/trimmed")
   sort(list.files(path_trim, pattern=".r1.fq", full.names = FALSE))-> R1.names
@@ -75,15 +73,11 @@ for (a in 1:ncol(AP)){
   sort(list.files(path_trim, pattern=".r2.fq", full.names = TRUE))-> R2
   paste0(path_trim, "/filtered_", R1.names)-> filtFs
   paste0(path_trim, "/filtered_", R2.names)-> filtRs
-
-
   #   這一步做pair，先用了r1的seq_along來迭代，可以改成指定數字，
-  for(i in seq_along(R1)) {
-    fastqPairedFilter(c(R1[i], R2[i]), c(filtFs[i], filtRs[i]),
-                      verbose=TRUE, matchIDs = TRUE)
-  }
-
-
+  #for(i in seq_along(R1)) {
+  #  fastqPairedFilter(c(R1[i], R2[i]), c(filtFs[i], filtRs[i]),
+  #                    verbose=TRUE, matchIDs = TRUE)
+  #}
 
   #pair好的檔案開始處理
   #pair reads
@@ -100,9 +94,6 @@ for (a in 1:ncol(AP)){
       # 核心運行，運行後看要不要merge，不能merge的就交由我寫的來處理
       dadaFs <- dada(r1, err=errF, multithread=TRUE)
       dadaRs <- dada(r2, err=errR, multithread=TRUE)
-
-
-
       paste0(rep(header, length(dadaFs[["clustering"]][["abundance"]])), "_", numbers[1:length(dadaFs[["clustering"]][["abundance"]])], rep("_r1_", length(dadaFs[["clustering"]][["abundance"]])), sprintf(dadaFs[["clustering"]][["abundance"]]/sum(dadaFs[["clustering"]][["abundance"]]), fmt = '%#.3f'), rep("_abundance_", length(dadaFs[["clustering"]][["abundance"]])), dadaFs[["clustering"]][["abundance"]])->r1list
       cbind(r1list,dadaFs[["clustering"]][["sequence"]],dadaFs[["clustering"]][["abundance"]])-> r1fas
       r1fas[order(as.numeric(r1fas[,3]), decreasing = TRUE),]-> r1fas
@@ -112,72 +103,74 @@ for (a in 1:ncol(AP)){
       r2fas[order(as.numeric(r2fas[,3]), decreasing = TRUE),]-> r2fas
       matrix(r2fas, ncol = 3)-> r2fas
 
+      write.table(r1fas[,1:2], file = paste0(path_demultiplex, "/denoice/r1/",filename), append = FALSE, sep = "\n", quote = FALSE,
+                  row.names = FALSE, col.names = FALSE)
+      write.table(r1fas[,1:2], file = paste0(path_demultiplex, "/denoice/r1/",r0), append = FALSE, sep = "\n", quote = FALSE,
+                  row.names = FALSE, col.names = FALSE)
+      write.table(r2fas[,1:2], file = paste0(path_demultiplex, "/denoice/r2/",filename), append = FALSE, sep = "\n", quote = FALSE,
+                  row.names = FALSE, col.names = FALSE)
+      write.table(r2fas[,1:2], file = paste0(path_demultiplex, "/denoice/r2/",r0), append = FALSE, sep = "\n", quote = FALSE,
+                  row.names = FALSE, col.names = FALSE)
+      #write.table(r2fas[1,1:2], file = paste0(path_demultiplex, "/denoice_best/r2/",filename), append = FALSE, sep = "\n", quote = FALSE, row.names = FALSE, col.names = FALSE)
+      #write.table(r1fas[1,1:2], file = paste0(path_demultiplex, "/denoice_best/r1/",filename), append = FALSE, sep = "\n", quote = FALSE, row.names = FALSE, col.names = FALSE)
+      #write.table(r1fas[1,1:2], file = paste0(path_demultiplex, "/denoice_best/r1/",r0), append = FALSE, sep = "\n", quote = FALSE,row.names = FALSE, col.names = FALSE)
+      #write.table(r2fas[1,1:2], file = paste0(path_demultiplex, "/denoice_best/r2/",r0), append = FALSE, sep = "\n", quote = FALSE, row.names = FALSE, col.names = FALSE)
+
       # r1 r2要merge了
-      try(mergers <- mergePairs(dadaFs, r1, dadaRs, r2, verbose=TRUE))
-
-
-
-
+      try(mergers <- mergePairs(dadaFs, r1, dadaRs, r2, minOverlap = minoverlap, verbose=TRUE))
       if (nrow(mergers)>0 & max(nchar(mergers$sequence))>AP_minlength & max(mergers$abundance)>20){
         sum(mergers$abundance)->clustersum
         paste0(rep(header, length(mergers$abundance)), "_", numbers[1:length(mergers$abundance)], rep("_", length(mergers$abundance)), sprintf(mergers$abundance/clustersum, fmt = '%#.3f'), rep("_abundance_", length(mergers$abundance)), mergers$abundance)->merglist
-        cbind(merglist,mergers$sequence,mergers$abundance)-> fas
-        fas[order(as.numeric(fas[,3]), decreasing = TRUE),] -> fas
-        matrix(fas, ncol = 3)-> fas
+        as.matrix(mergers)->mergers.table
+        nchar(mergers.table[,1]) -> seqlen #Kuo_modified
+        cbind(merglist,mergers.table,seqlen)-> fas0 #Kuo_modified
+        matrix(fas0, ncol = 11)->fas0
+        fas0[order(as.numeric(fas0[,3]), decreasing = TRUE),] -> fas0
+        matrix(fas0, ncol = 11)->fas0 #Kuo_modified may write out this matrix
+        rbind(merg, fas0)-> merg #Kuo_modified could be a table for dada2merge
+        fas0[as.numeric(fas0[,11])>AP_minlength,] -> fas #Kuo_modified
+        matrix(fas, ncol = 11)->fas
+        if (nrow(fas)==0){
+          cbind(r1, r2, header)->fail
+          rbind(dadamergfail, fail)-> dadamergfail
+        }
         write.table(fas[,1:2], file = paste0(path_demultiplex, "/denoice/", filename), append = FALSE, sep = "\n", quote = FALSE,
                     row.names = FALSE, col.names = FALSE)
         write.table(fas[1,1:2], file = paste0(path_demultiplex, "/denoice_best/",filename), append = FALSE, sep = "\n", quote = FALSE,
                     row.names = FALSE, col.names = FALSE)
-        write.table(r1fas[,1:2], file = paste0(path_demultiplex, "/denoice/r1/",filename), append = FALSE, sep = "\n", quote = FALSE,
-                    row.names = FALSE, col.names = FALSE)
-        write.table(r1fas[1,1:2], file = paste0(path_demultiplex, "/denoice_best/r1/",filename), append = FALSE, sep = "\n", quote = FALSE,
-                    row.names = FALSE, col.names = FALSE)
-        write.table(r1fas[,1:2], file = paste0(path_demultiplex, "/denoice/r1/",r0), append = FALSE, sep = "\n", quote = FALSE,
-                    row.names = FALSE, col.names = FALSE)
-        write.table(r1fas[1,1:2], file = paste0(path_demultiplex, "/denoice_best/r1/",r0), append = FALSE, sep = "\n", quote = FALSE,
-                    row.names = FALSE, col.names = FALSE)
-        write.table(r2fas[,1:2], file = paste0(path_demultiplex, "/denoice/r2/",filename), append = FALSE, sep = "\n", quote = FALSE,
-                    row.names = FALSE, col.names = FALSE)
-        write.table(r2fas[1,1:2], file = paste0(path_demultiplex, "/denoice_best/r2/",filename), append = FALSE, sep = "\n", quote = FALSE,
-                    row.names = FALSE, col.names = FALSE)
-        write.table(r2fas[,1:2], file = paste0(path_demultiplex, "/denoice/r2/",r0), append = FALSE, sep = "\n", quote = FALSE,
-                    row.names = FALSE, col.names = FALSE)
-        write.table(r2fas[1,1:2], file = paste0(path_demultiplex, "/denoice_best/r2/",r0), append = FALSE, sep = "\n", quote = FALSE,
-                    row.names = FALSE, col.names = FALSE)
-      }
-      else {
+      }else{
         cbind(r1, r2, header)->fail
-        rbind(mergfail, fail)-> mergfail
-        write.table(r1fas[,1:2], file = paste0(path_demultiplex, "/denoice/nonmerged/r1/",filename), append = FALSE, sep = "\n", quote = FALSE,
-                    row.names = FALSE, col.names = FALSE)
-        write.table(r1fas[1,1:2], file = paste0(path_demultiplex, "/denoice_best/nonmerged/r1/",filename), append = FALSE, sep = "\n", quote = FALSE,
-                    row.names = FALSE, col.names = FALSE)
-        write.table(r1fas[,1:2], file = paste0(path_demultiplex, "/denoice/nonmerged/r1/",r0), append = FALSE, sep = "\n", quote = FALSE,
-                    row.names = FALSE, col.names = FALSE)
-        write.table(r1fas[1,1:2], file = paste0(path_demultiplex, "/denoice_best/nonmerged/r1/",r0), append = FALSE, sep = "\n", quote = FALSE,
-                    row.names = FALSE, col.names = FALSE)
-        write.table(r2fas[,1:2], file = paste0(path_demultiplex, "/denoice/nonmerged/r2/",filename), append = FALSE, sep = "\n", quote = FALSE,
-                    row.names = FALSE, col.names = FALSE)
-        write.table(r2fas[1,1:2], file = paste0(path_demultiplex, "/denoice_best/nonmerged/r2/",filename), append = FALSE, sep = "\n", quote = FALSE,
-                    row.names = FALSE, col.names = FALSE)
-        write.table(r2fas[,1:2], file = paste0(path_demultiplex, "/denoice/nonmerged/r2/",r0), append = FALSE, sep = "\n", quote = FALSE,
-                    row.names = FALSE, col.names = FALSE)
-        write.table(r2fas[1,1:2], file = paste0(path_demultiplex, "/denoice_best/nonmerged/r2/",r0), append = FALSE, sep = "\n", quote = FALSE,
-                    row.names = FALSE, col.names = FALSE)
+        rbind(dadamergfail, fail)-> dadamergfail
       }
+
+      try(nonmergers <- mergePairs(dadaFs, r1, dadaRs, r2, verbose=TRUE, justConcatenate=TRUE))#Kuo_modified in following lines else
+      if (nrow(nonmergers)>0 ){
+        sum(nonmergers$abundance)->clustersum
+        paste0(rep(header, length(nonmergers$abundance)), "_", numbers[1:length(nonmergers$abundance)], rep("_", length(nonmergers$abundance)), sprintf(nonmergers$abundance/clustersum, fmt = '%#.3f'), rep("_abundance_", length(nonmergers$abundance)), nonmergers$abundance, rep("_10Ncat", length(nonmergers$abundance)))->nonmerglist
+        as.matrix(nonmergers)->nonmergers.table
+        cbind(nonmerglist,nonmergers.table)-> fascat #Kuo_modified
+        matrix(fascat, ncol = 10)->fascat
+        fascat[order(as.numeric(fascat[,3]), decreasing = TRUE),] -> fascat #Kuo_modified may write out this matrix
+        matrix(fascat, ncol = 10)->fascat
+        rbind(nonmerg, fascat)-> nonmerg #Kuo_modified could be a table for dada2nonmerge
+        write.table(fascat[,1:2], file = paste0(path_demultiplex, "/denoice/nonmerged/",filename), append = FALSE, sep = "\n", quote = FALSE,
+                    row.names = FALSE, col.names = FALSE)
+        write.table(fascat[1,1:2], file = paste0(path_demultiplex, "/denoice_best/nonmerged/",filename), append = FALSE, sep = "\n", quote = FALSE,
+                    row.names = FALSE, col.names = FALSE)
+        }
+
       cbind(rep(r1, length(dadaFs[["clustering"]][["abundance"]])),r1list)-> seqtable01
       cbind(rep(r2, length(dadaRs[["clustering"]][["abundance"]])),r2list)-> seqtable02
       rbind(seqtable, seqtable01, seqtable02)-> seqtable
     }
     if (purrr::has_element(list.files(path= path_trim),s1)==FALSE){
       cbind(r1, header)->mis
-      rbind(miss, mis)-> miss
+      rbind(miss, mis)-> miss}
     }
-  }
   write.table(miss, file = paste0(path_demultiplex, "/denoice/missing_samples.txt"), append = FALSE, sep = "\t", quote = FALSE,
               row.names = FALSE, col.names = FALSE)
   write.table(seqtable, file = paste0(path_demultiplex, "/denoice/sequence_table.txt"), append = FALSE, sep = "\t", quote = FALSE,
               row.names = FALSE, col.names = FALSE)
-  write.table(mergfail, file = paste0(path_demultiplex, "/denoice/merge_fail.txt"), append = FALSE, sep = "\t", quote = FALSE,
+  write.table(dadamergfail, file = paste0(path_demultiplex, "/denoice/dadamerge_fail.txt"), append = FALSE, sep = "\t", quote = FALSE,
               row.names = FALSE, col.names = FALSE)
 }
