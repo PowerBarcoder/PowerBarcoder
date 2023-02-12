43401b0240a9a62d550aced7cc2181f0446581c1
WINSTON\kwz50 <kwz50902@gmail.com>
2023-02-12 23:59:08 +0800

    [modify] rbcL 10N 53% 整理TODO，準備用trnLF接上全部pipline

--- main/powerBarcode.sh
+++ main/powerBarcode.sh
@@ -1,31 +1,31 @@
 # ! /bin/bash
 . ./config.sh
 
-#bash checkRequirement.sh
-#
-#echo "[INFO] environment OK !"
-#
-bash checkDirectory.sh
-#
-#echo "[INFO] file directories are created !"
-#
-#echo "[INFO] Start running PowerBarcode !"
-#
-## # -----------------------執行demultiplex-----------------------------
-#echo "[INFO] start illumina_PE_demultiplex"
-#bash illumina_PE_demultiplex_all_newprimer.sh
-#echo "[INFO] end illumina_PE_demultiplex"
-#
-## # -----------------------執行denoise-----------------------------
-#cd ${workingDirectory}
-#echo "[INFO] start dada2_denoise"
-#Rscript ${workingDirectory}dada2_denoise_PE_newprimer.r $ampliconInfo $workingDirectory $resultDataPath $dada2LearnErrorFile $dada2BarcodeFile ${nameOfLoci[@]} > ${resultDataPath}log_dada2.txt
-#echo "[INFO] end dada2_denoise"
+bash checkRequirement.sh
 
-# # -----------------------執行merge-----------------------------
+echo "[INFO] environment OK !"
+
+bash checkDirectory.sh
+
+echo "[INFO] file directories are created !"
+
+echo "[INFO] Start running PowerBarcode !"
+
+# # -----------------------執行demultiplex-----------------------------
+echo "[INFO] start illumina_PE_demultiplex"
+bash illumina_PE_demultiplex_all_newprimer.sh
+echo "[INFO] end illumina_PE_demultiplex"
+
+# # -----------------------執行denoise-----------------------------
 cd ${workingDirectory}
-echo "[INFO] start merge.sh"
-bash ${workingDirectory}merge.sh
-echo "[INFO] end merge.sh"
+echo "[INFO] start dada2_denoise"
+Rscript ${workingDirectory}dada2_denoise_PE_newprimer.r $ampliconInfo $workingDirectory $resultDataPath $dada2LearnErrorFile $dada2BarcodeFile ${nameOfLoci[@]} > ${resultDataPath}log_dada2.txt
+echo "[INFO] end dada2_denoise"
+
+## # -----------------------執行merge-----------------------------
+#cd ${workingDirectory}
+#echo "[INFO] start merge.sh"
+#bash ${workingDirectory}merge.sh
+#echo "[INFO] end merge.sh"
 
 echo "[INFO] end of flow"
