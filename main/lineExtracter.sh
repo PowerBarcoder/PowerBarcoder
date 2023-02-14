01375ac79032ca1e8465b39e7c03508f4f504d5b
winterdrive <90021888+winterdrive@users.noreply.github.com>
2023-02-14 19:36:57 +0800

    Update lineExtracter.sh

--- main/lineExtracter.sh
+++ main/lineExtracter.sh
@@ -2,10 +2,10 @@
 
 for file in /home2/analyses/BIFA/rbcL_for_merge/rbcLN_single/*; do
     base=$(basename "$file")
-    head -n 1 "$file" > "/home2/barcoder_test/RUN_sk_20230111_10N/PowerBarcoder/result_rbcL_final/rbcLN_demultiplex/denoice_best/nonmerged/r1/$base"
+    head -n 2 "$file" > "/home2/barcoder_test/RUN_sk_20230111_10N/PowerBarcoder/result_rbcL_final/rbcLN_demultiplex/denoice_best/nonmerged/r1/$base"
 done
 
 for file in /home2/analyses/BIFA/rbcL_for_merge/rbcLC_single/*; do
     base=$(basename "$file")
-    head -n 1 "$file" > "/home2/barcoder_test/RUN_sk_20230111_10N/PowerBarcoder/result_rbcL_final/rbcLN_demultiplex/denoice_best/nonmerged/r2/$base"
+    head -n 2 "$file" > "/home2/barcoder_test/RUN_sk_20230111_10N/PowerBarcoder/result_rbcL_final/rbcLN_demultiplex/denoice_best/nonmerged/r2/$base"
 done
