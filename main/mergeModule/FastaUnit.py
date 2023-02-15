3ce5ee2f16594632d838b5b8a1fb279a23f767ad
winston <winstontang@fontrip.com>
2023-02-15 15:42:09 +0800

    [modify] rbcLC 10N 57% add fastaSpliter in FastaUnit.py

--- main/mergeModule/FastaUnit.py
+++ main/mergeModule/FastaUnit.py
@@ -55,3 +55,66 @@ class FastaUnit:
             # seqMap = dict(zip(seqHeader, seqRead))
         self.seqMap = seqMap
         return self
+
+
+
+    """
+    get file name from path without extension
+    """
+    def getFileNameFromPath(path):
+        fileName = ""
+        if "/" in path:
+            fileName = path.split("/")[-1]
+        elif "\\" in path:
+            fileName = path.split("\\")[-1]
+        return ".".join(fileName.split(".")[:-1])
+
+
+    """
+    get file extension from path
+    """
+    def getFileExtensionFromPath(path):
+        extension = ""
+        if "/" in path:
+            extension = path.split(".")[-1]
+        return extension
+
+
+    """
+    Split a fasta with mulitple sequences into seperated file with one line header and one line sequence data.
+    Input supported for sequence data with multiple lines (such as the alignment result file in MAFFT).  
+    """
+    def splitMulitpleSeqFastaIntoFiles(self, loadPath, saveDir):
+        fileName = self.getFileNameFromPath(loadPath)
+        extension = self.getFileExtensionFromPath(loadPath)
+        count = len(open(loadPath).readlines())
+        print(count)
+        bufferedText = ""
+        fileSerialNumber=1
+        for i in range(1, count+1):
+            text = linecache.getline(loadPath, i)
+            x = text.replace("\n", "")
+            # print(x)
+            if ">" in x and i != 1:  # not first line header
+                with open(saveDir+fileName+"_"+str(fileSerialNumber)+"."+extension, "w") as file_edit:
+                    file_edit.write(bufferedText)
+                    fileSerialNumber+=1
+                bufferedText = ""
+                bufferedText+=(x+"\n")
+            elif ">" in x and i == 1: # first line header
+                bufferedText = ""
+                bufferedText+=(x+"\n")
+            elif ">" not in x and i != count: # not last line seq
+                bufferedText+=(x)
+            else: # last line seq
+                bufferedText+=(x)
+                with open(saveDir+fileName+"_"+str(fileSerialNumber)+"."+extension, "w") as file_edit:
+                    file_edit.write(bufferedText)
+                    fileSerialNumber+=1
+
+    # inputPath = 'C:/Users/kwz50/Desktop/i18n/workTool/seq/raw/KTHU2220_Wade5673_Tectaria_pleiosora_.fas'
+    # outputDir = 'C:/Users/kwz50/Desktop/i18n/workTool/seq/'
+    # inputPath2 = 'C:/Users/kwz50/Desktop/i18n/workTool/seq/raw/KTHU1778_Liu9858_Cephalomanes_oblongifolium_.fas_r2.al'
+    # outputDir2 = 'C:/Users/kwz50/Desktop/i18n/workTool/seq/'
+
+    # splitMulitpleSeqFastaIntoFiles(inputPath2, outputDir2)
