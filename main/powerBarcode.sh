fcc985aedf97480a1e49251f6cc8055b7ca1ef75
winterdrive <90021888+winterdrive@users.noreply.github.com>
2023-07-23 03:10:32 +0800

    [modified] renew dada2 identical 算法

--- main/powerBarcode.sh
+++ main/powerBarcode.sh
@@ -9,27 +9,27 @@ bash checkDirectory.sh "$1"
 echo "[INFO] file directories are created !"
 # # ----------------------------------------------------------
 
-#echo "[INFO] Start running PowerBarcode !"
-#
-## # -----------------------2. demultiplex---------------------
-#echo "[INFO] start illumina_PE_demultiplex"
-#bash illumina_PE_demultiplex_all_newprimer.sh "$1"
-#echo "[INFO] end illumina_PE_demultiplex"
-## # ----------------------------------------------------------
-#
-## # -----------------------3. denoise-------------------------
-#cd ${workingDirectory}
-#echo "[INFO] start dada2_denoise"
-#Rscript "${workingDirectory}dada2_denoise_PE_newprimer.r" "$ampliconInfo" "$workingDirectory" "$resultDataPath" "$dada2LearnErrorFile" "$dada2BarcodeFile" "$ampliconMinimumLength" "$minimunOverlapBasePair" "${nameOfLoci[@]}" "${primerFName[@]}" "${primerRName[@]}"
-#echo "[INFO] end dada2_denoise"
-## # ----------------------------------------------------------
-#
-## # -----------------------4. merge---------------------------
-#cd ${workingDirectory}
-#echo "[INFO] start merge.sh"
-#bash ${workingDirectory}merge.sh "$1"
-#echo "[INFO] end merge.sh"
-## # ----------------------------------------------------------
+echo "[INFO] Start running PowerBarcode !"
+
+# # -----------------------2. demultiplex---------------------
+echo "[INFO] start illumina_PE_demultiplex"
+bash illumina_PE_demultiplex_all_newprimer.sh "$1"
+echo "[INFO] end illumina_PE_demultiplex"
+# # ----------------------------------------------------------
+
+# # -----------------------3. denoise-------------------------
+cd ${workingDirectory}
+echo "[INFO] start dada2_denoise"
+Rscript "${workingDirectory}dada2_denoise_PE_newprimer.r" "$ampliconInfo" "$workingDirectory" "$resultDataPath" "$dada2LearnErrorFile" "$dada2BarcodeFile" "$ampliconMinimumLength" "$minimunOverlapBasePair" "${nameOfLoci[@]}" "${primerFName[@]}" "${primerRName[@]}"
+echo "[INFO] end dada2_denoise"
+# # ----------------------------------------------------------
+
+# # -----------------------4. merge---------------------------
+cd ${workingDirectory}
+echo "[INFO] start merge.sh"
+bash ${workingDirectory}merge.sh "$1"
+echo "[INFO] end merge.sh"
+# # ----------------------------------------------------------
 
 # # -----------------------5. QC------------------------------
 cd ${workingDirectory}
