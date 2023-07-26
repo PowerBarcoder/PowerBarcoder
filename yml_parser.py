2d9b6535232c584e72372efbc09fc7fd738e428f
winterdrive <90021888+winterdrive@users.noreply.github.com>
2023-07-26 23:55:18 +0800

    [modified] config.sh reformat

--- yml_parser.py
+++ yml_parser.py
@@ -23,6 +23,7 @@ def parsingYmlToShell(batch_name:str):
     # path
     script = '#!/bin/bash\n'
     script += "datetime='" + batch_name + "/'\n"
+    script += f"workingDirectory='/PowerBarcoder/main/'\n"
     script += f"myCutadaptPath='/venv/cutadapt-venv/bin/'\n"
     script += f"myFastpPath='/usr/local/bin/'\n"
     script += f"localBlastToolDir='/usr/local/bin/'\n"
@@ -37,60 +38,29 @@ def parsingYmlToShell(batch_name:str):
     script += f"dada2BarcodeFile='{str(config['dada2BarcodeFile']).strip()}'\n"  # dada2BarcodeFile
     script += f"ampliconMinimumLength='1'\n"
     script += f"minimunOverlapBasePair='4'\n"
-    script += f"devMode='0'\n"  # devMode (default: 0) 0: off, 1: on
+    # Dev Only
+    script += f"devMode='1'\n"  # devMode (default: 0) 0: off, 1: on
 
     # loci
     for i in range(len(config['nameOfLoci'])):
+        script += f"### loci:{str(config['nameOfLoci'][i]).strip()} ###\n"
         script += f"nameOfLoci+=('{str(config['nameOfLoci'][i]).strip()}')\n"  # nameOfLoci
-
-    for i in range(len(config['errorRateCutadaptor'])):
         script += f"errorRateCutadaptor+=('{str(config['errorRateCutadaptor'][i]).strip()}')\n"  # errorRateCutadaptor
-
-    for i in range(len(config['minimumLengthCutadaptor'])):
         script += f"minimumLengthCutadaptor+=('{str(config['minimumLengthCutadaptor'][i]).strip()}')\n"  # minimumLengthCutadaptor
-
-    for i in range(len(config['primerF'])):
         script += f"primerF+=('{str(config['primerF'][i]).strip()}')\n"  # primerF
-
-    for i in range(len(config['primerFName'])):
         script += f"primerFName+=('{str(config['primerFName'][i]).strip()}')\n"  # primerFName
-
-    for i in range(len(config['primerR'])):
         script += f"primerR+=('{str(config['primerR'][i]).strip()}')\n"  # primerR
-
-    for i in range(len(config['primerRName'])):
         script += f"primerRName+=('{str(config['primerRName'][i]).strip()}')\n"  # primerRName
-
-    # for i in range(len(config['amplicon_r1'])):
     #     script += f"amplicon_r1+=('{config['amplicon_r1'][i]}')\n"  # amplicon_r1
-    #
-    # for i in range(len(config['amplicon_r2'])):
     #     script += f"amplicon_r2+=('{config['amplicon_r2'][i]}')\n"  # amplicon_r2
-
-    for i in range(len(config['barcodesFile1'])):
         script += f"barcodesFile1+=('{str(config['barcodesFile1'][i]).strip()}')\n"  # barcodesFile1
-
-    for i in range(len(config['barcodesFile2'])):
         script += f"barcodesFile2+=('{str(config['barcodesFile2'][i]).strip()}')\n"  # barcodesFile2
-
-    for i in range(len(config['sseqidFileName'])):
         script += f"sseqidFileName+=('{str(config['sseqidFileName'][i]).strip()}')\n"  # sseqidFileName
-
-    for i in range(len(config['minimumLengthCutadaptorInLoop'])):
         script += f"minimumLengthCutadaptorInLoop+=('{str(config['minimumLengthCutadaptorInLoop'][i]).strip()}')\n"  # minimumLengthCutadaptorInLoop
-
-    for i in range(len(config['customizedCoreNumber'])):
         script += f"customizedCoreNumber+=('{str(config['customizedCoreNumber'][i]).strip()}')\n"  # customizedCoreNumber
-
-    # Dev Only
-    for i in range(len(config['nameOfLoci'])):
+        # Dev Only
         script += f"blastReadChoosingMode+=('1')\n" # blastReadChoosingMode (default: 1): 0: 10Ncat Blast, 1: split R1 R2 Blast
-
-    # Dev Only
-    for i in range(len(config['nameOfLoci'])):
         script += f"blastParsingMode+=('2')\n" # blastParsingMode (default: 2)
-
-    script += f"workingDirectory='/PowerBarcoder/main/'\n"
 
     script += 'echo \'[INFO] config imported!\'\n'
 
