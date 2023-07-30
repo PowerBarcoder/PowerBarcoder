5736dde9249716497f485deaff906fd925b0697a
winterdrive <90021888+winterdrive@users.noreply.github.com>
2023-07-30 20:22:07 +0800

    [modified] batch test

--- main/batchRun.sh
+++ main/batchRun.sh
@@ -2,13 +2,27 @@
 
 mkdir -p "/PowerBarcoder/data/result/batchRun"
 
-batchRunNumber=5
+batchRunNumber=30
 configFile="blasttest1"
 
 for ((i = 0; i <= batchRunNumber; i++)); do
-    mkdir -p "/PowerBarcoder/data/result/batchRun/blasttest$i"
-    bash powerBarcode.sh $configFile
-    cp -r /PowerBarcoder/data/result/blasttest1/* "/PowerBarcoder/data/result/batchRun/blasttest$i/"
-    find /PowerBarcoder/data/result/blasttest1 -type f ! -name "config.sh" -exec rm {} \;
+    targetDir="/PowerBarcoder/data/result/batchRun/blasttest$i"
+
+    # Check if the directory already exists
+    if [ ! -d "$targetDir" ]; then
+        # Create the directory if it doesn't exist
+        mkdir -p "$targetDir"
+
+        # Execute the bash script powerBarcode.sh using $configFile
+        bash powerBarcode.sh "$configFile"
+
+        # Copy the contents from /PowerBarcoder/data/result/blasttest1 to the target directory
+        cp -r /PowerBarcoder/data/result/blasttest1/* "$targetDir/"
+
+        # Remove all files in the target directory except "config.sh"
+        find "$targetDir" -type f ! -name "config.sh" -exec rm {} \;
+    else
+        echo "Directory $targetDir already exists. Skipping..."
+    fi
 done
 
