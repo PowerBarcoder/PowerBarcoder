d08cbbc78a807ba79f524b5f53ce44142e4724b6
winterdrive <90021888+winterdrive@users.noreply.github.com>
2024-02-11 01:59:31 +0800

    [trnLF] refactor the project structure

--- main/mergeModule/merge.sh
+++ main/mergeModule/merge.sh
@@ -5,13 +5,13 @@
 if [ "${blastReadChoosingMode}" -eq 1 ]; then
   echo "[INFO] blastReadChoosingMode is 1, run nnSpliter.py before blast "
   for ((i = 0; i < ${#nameOfLoci[@]}; i++)); do
-    python3 ./mergeModule/nnSpliter.py "$resultDataPath" "${nameOfLoci[i]}" #blast完，需要拆10N
+    python3 "${workingDirectory}"/mergeModule/nnSpliter.py "$resultDataPath" "${nameOfLoci[i]}" #blast完，需要拆10N
   done
 else #沒有的就跳過
   echo "[INFO] blastReadChoosingMode is 0, run nnSpliter.py after blast "
 fi
 
-bash ./mergeModule/00_blastForRef.sh "$1" #先blast，內部自帶迴圈處理
+bash "${workingDirectory}"/mergeModule/00_blastForRef.sh "$1" #先blast，內部自帶迴圈處理
 
 for ((i = 0; i < ${#nameOfLoci[@]}; i++)); do
   count=0
@@ -25,17 +25,17 @@ for ((i = 0; i < ${#nameOfLoci[@]}; i++)); do
     if [ "${blastReadChoosingMode}" -eq 0 ]; then
       # r1r2 cat起來blast
       #  就是這裡拆NN到原先的nonmerged/r1,r2資料夾裡(只負責拆，判斷方向交給下一步驟)
-      python3 ./mergeModule/nnSpliter.py "$resultDataPath" "${nameOfLoci[i]}" #blast完，需要拆10N
+      python3 "${workingDirectory}"/mergeModule/nnSpliter.py "$resultDataPath" "${nameOfLoci[i]}" #blast完，需要拆10N
     fi
 
     #  準備parsing各loci local blast的結果 --- input：10N seq 的 blast refResult; output：10N blastResult
-    python3 ./mergeModule/blastResultParser.py "$ampliconInfo" "$resultDataPath" "${blastParsingMode[i]}" "${nameOfLoci[i]}"
+    python3 "${workingDirectory}"/mergeModule/blastResultParser.py "$ampliconInfo" "$resultDataPath" "${blastParsingMode[i]}" "${nameOfLoci[i]}"
 
     # 20230107 10N，需按blast的結果做reverse complement後，輸出正確方向的ref(改動的部分不太確定有沒有對，因為這整批好像都沒有在做reverse compliment)
     # 20230215 10N，reverse complement已確認是只轉ref的部分
-    python3 ./mergeModule/alignmentPretreater.py "$ampliconInfo" "${sseqidFileName[i]}" "$resultDataPath" "${nameOfLoci[i]}"
-    python3 ./mergeModule/alignmenter.py "$ampliconInfo" "$resultDataPath" "${nameOfLoci[i]}"
-    python3 ./mergeModule/merger.py "$ampliconInfo" "$resultDataPath" "${nameOfLoci[i]}"
+    python3 "${workingDirectory}"/mergeModule/alignmentPretreater.py "$ampliconInfo" "${sseqidFileName[i]}" "$resultDataPath" "${nameOfLoci[i]}"
+    python3 "${workingDirectory}"/mergeModule/alignmenter.py "$ampliconInfo" "$resultDataPath" "${nameOfLoci[i]}"
+    python3 "${workingDirectory}"/mergeModule/merger.py "$ampliconInfo" "$resultDataPath" "${nameOfLoci[i]}"
 
   else #沒有的就跳過
     echo "[WARNING] no nonmerged files found in ${nameOfLoci[i]}"
--- main/powerBarcode.sh
+++ main/powerBarcode.sh
@@ -3,9 +3,9 @@
 . /PowerBarcoder/data/result/"$1"/config.sh
 
 # # -----------------------1. prepare env.---------------------
-bash checkRequirement.sh "$1"
+bash ./envCheckModule/checkRequirement.sh "$1"
 echo "[INFO] environment OK !"
-bash checkDirectory.sh "$1"
+bash ./envCheckModule/checkDirectory.sh "$1"
 echo "[INFO] file directories are created !"
 # # ----------------------------------------------------------
 
@@ -13,35 +13,35 @@ echo "[INFO] Start running PowerBarcode !"
 
 # # -----------------------2. demultiplex---------------------
 echo "[INFO] start illumina_PE_demultiplex"
-bash illumina_PE_demultiplex_all_newprimer.sh "$1"
+bash ./demultiplexModule/illumina_PE_demultiplex_all_newprimer.sh "$1"
 echo "[INFO] end illumina_PE_demultiplex"
 # # ----------------------------------------------------------
 
 # # -----------------------3. denoise-------------------------
 cd ${workingDirectory}
 echo "[INFO] start dada2_denoise"
-Rscript "${workingDirectory}dada2_denoise_PE_newprimer.r" "$ampliconInfo" "$workingDirectory" "$resultDataPath" "$dada2LearnErrorFile" "$dada2BarcodeFile" "$amplicon_minimum_length" "$minimum_overlap_base_pair" "${nameOfLoci[@]}" "${primerFName[@]}" "${primerRName[@]}"
+Rscript "${workingDirectory}/denoiseModule/dada2_denoise_PE_newprimer.r" "$ampliconInfo" "$workingDirectory" "$resultDataPath" "$dada2LearnErrorFile" "$dada2BarcodeFile" "$amplicon_minimum_length" "$minimum_overlap_base_pair" "${nameOfLoci[@]}" "${primerFName[@]}" "${primerRName[@]}"
 echo "[INFO] end dada2_denoise"
 # # ----------------------------------------------------------
 
 # # -----------------------4. merge---------------------------
 cd ${workingDirectory}
 echo "[INFO] start merge.sh"
-bash ${workingDirectory}merge.sh "$1"
+bash ${workingDirectory}/mergeModule/merge.sh "$1"
 echo "[INFO] end merge.sh"
 # # ----------------------------------------------------------
 
 # # -----------------------5. QC------------------------------
 cd ${workingDirectory}
 echo "[INFO] start qc.sh"
-bash ${workingDirectory}qc.sh "$1"
+bash ${workingDirectory}/qcModule/qc.sh "$1"
 echo "[INFO] end qc.sh"
 # # ----------------------------------------------------------
 
 # # -----------------------6. dev mode------------------------
 cd ${workingDirectory}
 echo "[INFO] start dev.sh"
-bash ${workingDirectory}dev.sh "$1"
+bash ${workingDirectory}/housekeepingModule/dev.sh "$1"
 echo "[INFO] end dev.sh"
 # # ----------------------------------------------------------
 
