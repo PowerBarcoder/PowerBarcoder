3030c9b2ae4caafd737707e2d1d936aac27bb409
winterdrive <90021888+winterdrive@users.noreply.github.com>
2024-02-14 17:15:12 +0800

    [trnLF] remove restart policy in docker command

--- main/poc/cpu_test.sh
+++ main/poc/cpu_test.sh
@@ -9,12 +9,12 @@
 #done
 
 # Run the Docker container with the specified number of CPU cores
-docker run -d -p 5000:5000 -v ${PWD}:/PowerBarcoder --name powerbarcoder_2--cpus=2 powerbarcoder
-docker run -d -p 5000:5000 -v ${PWD}:/PowerBarcoder --name powerbarcoder_4--cpus=4 powerbarcoder
-docker run -d -p 5000:5000 -v ${PWD}:/PowerBarcoder --name powerbarcoder_8--cpus=8 powerbarcoder
-docker run -d -p 5000:5000 -v ${PWD}:/PowerBarcoder --name powerbarcoder_16--cpus=16 powerbarcoder
-docker run -d -p 5000:5000 -v ${PWD}:/PowerBarcoder --name powerbarcoder_24--cpus=24 powerbarcoder
-docker run -d -p 5000:5000 -v ${PWD}:/PowerBarcoder --name powerbarcoder_32--cpus=32 powerbarcoder
+docker run -d -p 5000:5000 -v ${PWD}:/PowerBarcoder --name powerbarcoder_2 --cpus=2 powerbarcoder
+docker run -d -p 5000:5000 -v ${PWD}:/PowerBarcoder --name powerbarcoder_4 --cpus=4 powerbarcoder
+docker run -d -p 5000:5000 -v ${PWD}:/PowerBarcoder --name powerbarcoder_8 --cpus=8 powerbarcoder
+docker run -d -p 5000:5000 -v ${PWD}:/PowerBarcoder --name powerbarcoder_16 --cpus=16 powerbarcoder
+docker run -d -p 5000:5000 -v ${PWD}:/PowerBarcoder --name powerbarcoder_24 --cpus=24 powerbarcoder
+docker run -d -p 5000:5000 -v ${PWD}:/PowerBarcoder --name powerbarcoder_32 --cpus=32 powerbarcoder
 
 #This might not be necessary
-docker run -d -p 5000:5000 -v ${PWD}:/PowerBarcoder --name powerbarcoder_36--cpus=36 powerbarcoder
+docker run -d -p 5000:5000 -v ${PWD}:/PowerBarcoder --name powerbarcoder_36 --cpus=36 powerbarcoder
