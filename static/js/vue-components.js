d332b9e3f9a2fc9411cba7711069ea1897c68b67
Tang, Sheng-Kai <90021888+winterdrive@users.noreply.github.com>
2025-03-05 00:09:28 +0800

    Add Vue component for modal in `vue-components.js`
    * Create a Vue component named 'modal' with props for title, darkMode, and isDownloadEnabled
    * Define template for modal structure including header, body, and footer
    * Implement methods for loadPrevious, clearLog, saveLog, and downloadResult
    * Initialize Vue instance to manage form data and socket communication

--- static/js/vue-components.js
+++ static/js/vue-components.js
@@ -19,6 +19,85 @@ Vue.component('form-group', {
     }
 });
 
+Vue.component('modal', {
+    props: ['title', 'darkMode', 'isDownloadEnabled'],
+    template: `
+        <div class="modal" id="myModal">
+            <div class="modal-dialog modal-fullscreen">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title">{{ title }}</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body" id="modalBodyDiv" style="padding-bottom: 0rem">
+                        <div class="text-center mb-3"></div>
+                        <div id="consoleDiv">
+                            <pre style="margin-bottom: 0rem" id="console">
+                                <div class="d-flex justify-content-center" id="loadPreviousDiv">
+                                    <button class="btn btn-success" id="loadPreviousButton" style="display: none;" @click="loadPrevious">Load Previous</button>
+                                </div>
+                            </pre>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-sm">
+                                    <span>&nbsp</span>
+                                    <div class="form-check form-switch">
+                                        <input class="form-check-input" type="checkbox" role="switch" id="darkModeToggle" v-model="darkMode">
+                                        <label class="form-check-label" for="darkModeToggle">Dark Mode</label>
+                                    </div>
+                                </div>
+                                <div class="col-sm d-md-flex justify-content-md-end">
+                                    <button class="btn btn-warning" id="downloadButton" :disabled="!isDownloadEnabled" @click="downloadResult">Download Result</button>
+                                    &nbsp;
+                                    <button class="btn btn-secondary" id="clearButton" @click="clearLog">Clear Log</button>
+                                    &nbsp;
+                                    <button class="btn btn-primary" id="saveButton" @click="saveLog">Save Execution Log</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    `,
+    methods: {
+        loadPrevious() {
+            // Implement load previous functionality
+        },
+        clearLog() {
+            $("#console").children().not("#loadPreviousDiv").remove();
+        },
+        saveLog() {
+            let removedLogContent = removedData.join('\n') + '\n';
+            let codeElement = document.getElementById('console');
+            let codeElementCopy = codeElement.cloneNode(true);
+            let divToRemove = codeElementCopy.querySelector("#loadPreviousDiv");
+            if (divToRemove) {
+                codeElementCopy.removeChild(divToRemove);
+            }
+            let innerText = (removedLogContent + codeElementCopy.innerText)
+                .replaceAll("                ", "")
+                .replace("                    [", "[")
+                .replaceAll("<", "&lt;")
+                .replaceAll(">", "&gt;");
+            let downloadLink = document.createElement('a');
+            downloadLink.href = 'data:text/plain;charset=utf-8,' + encodeURIComponent(innerText);
+            downloadLink.download = 'log.txt';
+            downloadLink.style.display = 'none';
+            document.body.appendChild(downloadLink);
+            downloadLink.click();
+            document.body.removeChild(downloadLink);
+        },
+        downloadResult() {
+            let roomName = sessionStorage.getItem('roomName');
+            window.open(HOST_URL + 'download/' + roomName, '_blank');
+        }
+    }
+});
+
 new Vue({
     el: '#app',
     data: {
