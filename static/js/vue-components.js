7b9e9fdd84f0362f6f4c26c125a84ea2b2b9db4e
Tang, Sheng-Kai <90021888+winterdrive@users.noreply.github.com>
2025-03-05 00:00:11 +0800

    Introduce Vue to the frontend with current flask app
    Introduce Vue.js to the frontend of the Flask app.
    
    * **Add Vue.js CDN**:
      - Add Vue.js CDN link in the head section of `templates/index.html`.
    
    * **Update HTML structure**:
      - Wrap the form and modal elements in a new div with id `app`.
      - Update form and modal elements to use Vue directives for data binding and event handling.
    
    * **Form management**:
      - Update form elements to use Vue components for data binding and event handling.
      - Replace existing form-related JavaScript code with Vue components.
    
    * **Modal management**:
      - Update modal elements to use Vue components for data binding and event handling.
      - Replace existing modal-related JavaScript code with Vue components.
    
    * **Initialize Vue instance**:
      - Initialize a Vue instance to manage form data and socket communication.
    
    * **Update JavaScript file**:
      - Update `static/js/scripts.js` to initialize Vue instance from `static/js/vue-components.js`.
    
    * **Add new JavaScript file**:
      - Add `static/js/vue-components.js` to define Vue components for form and modal.
    
    ---
    
    For more details, open the [Copilot Workspace session](https://copilot-workspace.githubnext.com/PowerBarcoder/PowerBarcoder?shareId=XXXX-XXXX-XXXX-XXXX).

---
+++ static/js/vue-components.js
@@ -0,0 +1,210 @@
+Vue.component('form-group', {
+    props: ['label', 'tooltip', 'value', 'placeholder', 'class'],
+    template: `
+        <div :class="['form-group', class]">
+            <label :for="label">{{ label }}</label>
+            <input type="text" class="form-control" :id="label" :placeholder="placeholder" v-model="inputValue">
+            <span :id="'tooltip' + label" class="fa fa-info-circle" data-bs-toggle="tooltip" :title="tooltip"></span>
+        </div>
+    `,
+    data() {
+        return {
+            inputValue: this.value
+        };
+    },
+    watch: {
+        inputValue(newValue) {
+            this.$emit('input', newValue);
+        }
+    }
+});
+
+new Vue({
+    el: '#app',
+    data: {
+        formData: {
+            ampliconInfo: '',
+            R1FastqGz: '',
+            R2FastqGz: '',
+            dada2LearnErrorFile: '',
+            dada2BarcodeFile: '',
+            dev_mode: '',
+            denoise_mode: '',
+            amplicon_minimum_length: '',
+            nameOfLoci: [''],
+            errorRateCutadapt: [''],
+            minimumLengthCutadapt: [''],
+            primerFName: [''],
+            primerF: [''],
+            primerRName: [''],
+            primerR: [''],
+            barcodesFile1: [''],
+            barcodesFile2: [''],
+            sseqidFileName: [''],
+            minimumLengthCutadaptInLoop: [''],
+            customizedCoreNumber: [''],
+            blastReadChoosingMode: [''],
+            blastParsingMode: ['']
+        },
+        isSubmitting: false,
+        submitButtonText: 'Submit',
+        darkMode: false,
+        isDownloadEnabled: false
+    },
+    methods: {
+        handleFormSubmission() {
+            this.isSubmitting = true;
+            this.submitButtonText = 'Please wait...';
+
+            const formData = this.formData;
+
+            $("#myModal").modal("show");
+            $("#showModalButton").removeClass("d-none");
+            socket.emit('run-procedure', formData);
+
+            let seconds = 70;
+            let countdownInterval = setInterval(() => {
+                seconds--;
+                if (seconds > 0) {
+                    this.submitButtonText = `Please wait... ${seconds}s`;
+                } else {
+                    this.isSubmitting = false;
+                    this.submitButtonText = 'Submit';
+                    clearInterval(countdownInterval);
+                }
+            }, 1000);
+        },
+        openAdvanceMode() {
+            const advanceModeElements = document.querySelectorAll('.advanceMode');
+            advanceModeElements.forEach(element => {
+                element.classList.toggle('hideAdvanceMode');
+                element.classList.toggle('showAdvanceMode');
+            });
+        },
+        autoCompleteWithDefaultPath() {
+            this.formData.ampliconInfo = CONFIG.default_amplicon_info;
+            this.formData.R1FastqGz = CONFIG.default_r1_fastq_gz;
+            this.formData.R2FastqGz = CONFIG.default_r2_fastq_gz;
+            this.formData.dada2LearnErrorFile = CONFIG.default_dada2_learn_error_file;
+            this.formData.dada2BarcodeFile = CONFIG.default_dada2_barcode_file;
+            this.formData.dev_mode = CONFIG.dev_mode;
+            this.formData.amplicon_minimum_length = CONFIG.amplicon_minimum_length;
+        },
+        autoCompleteWithRBCLLoci() {
+            this.formData.nameOfLoci[0] = CONFIG.rbcl_name_of_loci;
+            this.formData.primerF[0] = CONFIG.rbcl_primer_f;
+            this.formData.primerR[0] = CONFIG.rbcl_primer_r;
+            this.formData.barcodesFile1[0] = CONFIG.rbcl_barcodes_file1;
+            this.formData.barcodesFile2[0] = CONFIG.rbcl_barcodes_file2;
+            this.formData.sseqidFileName[0] = CONFIG.rbcl_sseqid_file_name;
+            this.formData.errorRateCutadapt[0] = CONFIG.rbcl_error_rate_cutadapt;
+            this.formData.minimumLengthCutadapt[0] = CONFIG.rbcl_minimum_length_cutadapt;
+            this.formData.minimumLengthCutadaptInLoop[0] = CONFIG.rbcl_minimum_length_cutadapt_in_loop;
+            this.formData.customizedCoreNumber[0] = CONFIG.rbcl_customized_core_number;
+            this.formData.minimum_overlap_base_pair[0] = CONFIG.rbcl_minimum_overlap_base_pair;
+            this.formData.maximum_mismatch_base_pair[0] = CONFIG.rbcl_maximum_mismatch_base_pair;
+            this.formData.blastReadChoosingMode[0] = CONFIG.rbcl_blast_read_choosing_mode;
+            this.formData.blastParsingMode[0] = CONFIG.rbcl_blast_parsing_mode;
+        },
+        autoCompleteWithTRNLFLoci() {
+            this.formData.nameOfLoci[0] = CONFIG.trnlf_name_of_loci;
+            this.formData.primerF[0] = CONFIG.trnlf_primer_f;
+            this.formData.primerR[0] = CONFIG.trnlf_primer_r;
+            this.formData.barcodesFile1[0] = CONFIG.trnlf_barcodes_file1;
+            this.formData.barcodesFile2[0] = CONFIG.trnlf_barcodes_file2;
+            this.formData.sseqidFileName[0] = CONFIG.trnlf_sseqid_file_name;
+            this.formData.errorRateCutadapt[0] = CONFIG.trnlf_error_rate_cutadapt;
+            this.formData.minimumLengthCutadapt[0] = CONFIG.trnlf_minimum_length_cutadapt;
+            this.formData.minimumLengthCutadaptInLoop[0] = CONFIG.trnlf_minimum_length_cutadapt_in_loop;
+            this.formData.customizedCoreNumber[0] = CONFIG.trnlf_customized_core_number;
+            this.formData.minimum_overlap_base_pair[0] = CONFIG.trnlf_minimum_overlap_base_pair;
+            this.formData.maximum_mismatch_base_pair[0] = CONFIG.trnlf_maximum_mismatch_base_pair;
+            this.formData.blastReadChoosingMode[0] = CONFIG.trnlf_blast_read_choosing_mode;
+            this.formData.blastParsingMode[0] = CONFIG.trnlf_blast_parsing_mode;
+        },
+        addNewLocus() {
+            this.formData.nameOfLoci.push('');
+            this.formData.primerF.push('');
+            this.formData.primerR.push('');
+            this.formData.barcodesFile1.push('');
+            this.formData.barcodesFile2.push('');
+            this.formData.sseqidFileName.push('');
+            this.formData.errorRateCutadapt.push('');
+            this.formData.minimumLengthCutadapt.push('');
+            this.formData.minimumLengthCutadaptInLoop.push('');
+            this.formData.customizedCoreNumber.push('');
+            this.formData.minimum_overlap_base_pair.push('');
+            this.formData.maximum_mismatch_base_pair.push('');
+            this.formData.blastReadChoosingMode.push('');
+            this.formData.blastParsingMode.push('');
+        },
+        showModal() {
+            $("#myModal").modal("show");
+        },
+        loadPrevious() {
+            // Implement load previous functionality
+        },
+        clearLog() {
+            $("#console").children().not("#loadPreviousDiv").remove();
+        },
+        saveLog() {
+            let removedLogContent = removedData.join('\n') + '\n';
+            let codeElement = document.getElementById('console');
+            let codeElementCopy = codeElement.cloneNode(true);
+            let divToRemove = codeElementCopy.querySelector("#loadPreviousDiv");
+            if (divToRemove) {
+                codeElementCopy.removeChild(divToRemove);
+            }
+            let innerText = (removedLogContent + codeElementCopy.innerText)
+                .replaceAll("                ", "")
+                .replace("                    [", "[")
+                .replaceAll("<", "&lt;")
+                .replaceAll(">", "&gt;");
+            let downloadLink = document.createElement('a');
+            downloadLink.href = 'data:text/plain;charset=utf-8,' + encodeURIComponent(innerText);
+            downloadLink.download = 'log.txt';
+            downloadLink.style.display = 'none';
+            document.body.appendChild(downloadLink);
+            downloadLink.click();
+            document.body.removeChild(downloadLink);
+        },
+        downloadResult() {
+            let roomName = sessionStorage.getItem('roomName');
+            window.open(HOST_URL + 'download/' + roomName, '_blank');
+        }
+    },
+    mounted() {
+        let socket = io.connect(HOST_URL, {
+            withCredentials: true,
+        });
+
+        socket.on('procedure-result', (data) => {
+            let newDiv = $("<code>");
+            newDiv.text(data);
+            $("#console").append(newDiv);
+            newDiv.addClass("shellsession");
+
+            if (this.darkMode) {
+                hljs.highlightAll();
+            }
+
+            let lines = $("#console").find("code").length;
+            if (lines > 50) {
+                let firstLine = $("#console").find("code:first");
+                let removedLine = firstLine.text().trim();
+                removedData.push(removedLine);
+                firstLine.remove();
+                $("#loadPreviousButton").show();
+            }
+
+            if (data.includes("Socket room name:")) {
+                let roomName = data.split(":")[1].trim();
+                sessionStorage.setItem('roomName', roomName);
+            }
+
+            if (data.includes("Find your results in")) {
+                this.isDownloadEnabled = true;
+            }
+        });
+    }
+});
